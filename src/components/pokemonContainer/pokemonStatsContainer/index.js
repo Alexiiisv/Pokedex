4af930b54e79935914b0bf425b7f9a0fe54adbde
Alexis VELLEINE <alexis.velleine@ynov.com>
2023-01-04 16:24:36 +0100

    finition de l'affichage des stats de base du pokemon

--- src/components/pokemonContainer/pokemonStatsContainer/index.js
+++ src/components/pokemonContainer/pokemonStatsContainer/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import {Text} from 'react-native';
-import {AlignCenter, MainLabel, PokemonContainer, PokemonTC} from './style';
+import {
+  AlignCenter,
+  CompProgressBar,
+  Label,
+  MainLabel,
+  PokemonContainer,
+  PokemonTC,
+  ProgressBarContainer,
+} from './style';
+import {MD3Colors} from 'react-native-paper';
 
 export const PokemonStatsContainer = ({pokemon}) => {
   const Section = datainfos => {
@@ -12,14 +20,22 @@ export const PokemonStatsContainer = ({pokemon}) => {
     datainfos.stat.name =
       datainfos.stat.name.charAt(0).toUpperCase() +
       datainfos.stat.name.slice(1);
+    // datainfos.base_stat = Math.round(Math.random() * 255); //si on veux des valeurs random pour les stats de base du pokemon
+    var progressBarValue = (datainfos.base_stat * 1) / 255;
     return (
       <PokemonTC key={datainfos.stat.name}>
-        <Text>{datainfos.stat.name}:</Text>
-        <Text>{datainfos.base_stat}</Text>
+        <Label width={'22%'}>{datainfos.stat.name}: </Label>
+        <Label width={'12%'}>{datainfos.base_stat} </Label>
+        <ProgressBarContainer>
+          <CompProgressBar
+            progress={progressBarValue}
+            visible={true}
+            color={MD3Colors.error50}
+          />
+        </ProgressBarContainer>
       </PokemonTC>
     );
   };
-  console.log(pokemon.pokemon.url.stats);
   return (
     <AlignCenter>
       <MainLabel>Base stats</MainLabel>
--- src/components/pokemonContainer/pokemonStatsContainer/style.js
+++ src/components/pokemonContainer/pokemonStatsContainer/style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import {ProgressBar} from 'react-native-paper';
 
 export const PokemonContainer = styled.View`
   width: 100%;
@@ -10,6 +11,7 @@ export const AlignCenter = styled.View`
 
 export const PokemonTC = styled.View`
   align-items: center;
+  justify-content: center;
   flex-direction: row;
 `;
 
@@ -17,4 +19,21 @@ export const MainLabel = styled.Text`
   margin-top: 5px;
   font-size: 15px;
   color: white;
+`;
+
+export const Label = styled.Text`
+  width: ${p => p.width};
+  color: white;
+  font-size: 18px;
+`;
+
+export const ProgressBarContainer = styled.View`
+  margin-top: 10px;
+  margin-bottom: 10px;
+  width: 50%;
+`;
+
+export const CompProgressBar = styled(ProgressBar)`
+  background-color: black;
+  height: 20px;
 `;
